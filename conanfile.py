e2f6ee3bb7947ef0e67d6e317c94931db1965310
Kalle R. Møller <git@k-moeller.dk>
2017-05-14 16:11:30 +0200

    Small fixes

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,25 @@
-from conans import ConanFile, tools, ConfigureEnvironment
+from conans import ConanFile, tools
 import os
 
-
-class StrawberryPerlinstallerConan(ConanFile):
-    name = "strawberry_perl_installer"
+class StrawberryperlinstallerConan(ConanFile):
+    name = "strawberryperl_installer"
     version = "0.1"
     license = "MIT"
-    url = "http://github.com/KalleDK/conan-strawberryperl-installer"
+    url = "https://github.com/KalleDK/conan-strawberryperl-installer"
     settings = {"os": ["Windows"]}
-   
+
     def build(self):
-        keychain = "%s_%s_%s_%s" % ("5.24", "x86_64")
-                                    
+        keychain = "%s_%s" % ("5.24", "x86_64")
+        
         files = {"5.24_x86_64": "http://strawberryperl.com/download/5.24.1.1/strawberry-perl-5.24.1.1-64bit-portable.zip"}
         
         tools.download(files[keychain], "file.zip")
         tools.unzip("file.zip", "dist")
-    
+
     def package(self):
         self.copy("*", dst="", src="dist")
 
     def package_info(self):
-        self.env_info.path.append(os.path.join(self.package_folder, "c\bin"))
-        self.env_info.path.append(os.path.join(self.package_folder, "perl\site\bin"))
-        self.env_info.path.append(os.path.join(self.package_folder, "perl\bin"))
+        self.env_info.path.append(os.path.join(self.package_folder, "c", "bin"))
+        self.env_info.path.append(os.path.join(self.package_folder, "perl", "site", "bin"))
+        self.env_info.path.append(os.path.join(self.package_folder, "perl", "bin"))
